be109a0f9f497c2d25424d5c247445921196209f
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2026-02-09 02:12:03 +0800

    feat: add komodo

---
+++ modules/cluster/komodo.nix
@@ -0,0 +1,38 @@
+{
+  flake.modules.nixos.base =
+    {
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      virtualisation = {
+        containers.enable = true;
+        docker.enable = lib.mkForce false;
+        podman = {
+          enable = true;
+          dockerCompat = true;
+          defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+        };
+      };
+
+      environment.systemPackages = [
+        pkgs.openssl
+      ];
+      systemd.services.komodo-periphery.serviceConfig.SupplementaryGroups = lib.mkForce [ "podman" ];
+      users.users.komodo-periphery.extraGroups = lib.mkForce [ "docker" ];
+    };
+  flake.modules.nixos.mininas =
+    {
+      config,
+      ...
+    }:
+    {
+      services.komodo-periphery = {
+        enable = true;
+        allowedIps = [ "127.0.0.1" ];
+      };
+    };
+
+}
